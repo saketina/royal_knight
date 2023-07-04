ad0f0d9e443a4e7887c493a65a5ed24aeb5e04dc
Saketina <deni.dobrijevic@gmail.com>
2023-07-04 15:23:42 +0200

    transfer of dev cmds

---
+++ cogs/dev.py
@@ -0,0 +1,87 @@
+import disnake
+from disnake.ext import commands, tasks
+from disnake.ext.commands import guild_only, is_owner
+import json
+
+import asyncio
+import aiofiles
+import typing
+import time
+import pyrebase
+
+firebase = pyrebase.initialize_app(
+    json.load(open("firebase_config.json", "r")))
+db = firebase.database()
+
+
+class Utility(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        
+    @commands.command(name='eval', pass_context=True)
+    @commands.is_owner()
+    async def eval_command(self, ctx, *, expr):
+        try:
+            if 'await ' in expr:
+                new_expr = expr.replace('await ', '')
+                ans = await eval(new_expr)
+            else:
+                ans = eval(expr)
+            await ctx.send(f"Answer: {ans}")
+        except Exception as e:
+            print(e.__traceback__)
+            await ctx.send("Didn't work.")
+
+    @commands.command(name="toggle", pass_context=True)
+    @commands.is_owner()
+    async def toggle(self, ctx, *, command):
+        command = self.client.get_command(command)
+
+        if command is None:
+            embed = disnake.Embed(title="ERROR", description="I can't find a command with that name!", color=0xff0000)
+            await ctx.send(embed=embed)
+
+        elif ctx.command == command:
+            embed = disnake.Embed(title="ERROR", description="You cannot disable this command.", color=0xff0000)
+            await ctx.send(embed=embed)
+
+        else:
+            command.enabled = not command.enabled
+            ternary = "enabled" if command.enabled else "disabled"
+            embed = disnake.Embed(title="Toggle", description=f"I have {ternary} {command.qualified_name} for you!", color=0xff00c8)
+            await ctx.send(embed=embed)
+
+    @commands.command(pass_context=True)
+    @commands.is_owner()
+    async def leave(self, ctx, *, guildinput):
+        try:
+            guildid = int(guildinput)
+        except:
+            await ctx.send("Invalid guild: failed to convert to int")
+
+        try:
+            guild = self.client.get_guild(guildid)
+        except:
+            await ctx.send("Invalid guild")
+
+        try:
+            await guild.leave()
+            await ctx.send(f"left {guild.name}")
+        except:
+            await ctx.send("Error leaving")
+
+    @commands.command(pass_context=True)
+    @commands.is_owner()
+    async def servers(self, ctx):
+        await ctx.send(f"{len(self.client.guilds)}")
+        listofids = []
+        for guild in self.client.guilds:
+            listofids.append(guild.id)
+        await ctx.send(listofids)
+
+def setup(client):
+    client.add_cog(Dev(client))
+    print(f"Cog: Dev - loaded.")
+
+def teardown(client):
+    print(f"Cog: Dev - unloaded.")
--- cogs/utility.py
+++ cogs/utility.py
@@ -17,7 +17,6 @@ db = firebase.database()
 class Utility(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.afk_dict = {}
 
     @commands.slash_command(name = "hello", description="Greets the user.")
     async def hello(self, ctx):
@@ -41,20 +40,6 @@ class Utility(commands.Cog):
     @commands.command()
     async def serverinfo(self, ctx):
         await ctx.send(ctx.guild.icon)
-
-    @commands.command(name='eval', pass_context=True)
-    @commands.is_owner()
-    async def eval_command(self, ctx, *, expr):
-        try:
-            if 'await ' in expr:
-                new_expr = expr.replace('await ', '')
-                ans = await eval(new_expr)
-            else:
-                ans = eval(expr)
-            await ctx.send(f"Answer: {ans}")
-        except Exception as e:
-            print(e.__traceback__)
-            await ctx.send("Didn't work.")
 
     @commands.command(pass_context=True)
     @commands.guild_only()
@@ -165,53 +150,6 @@ class Utility(commands.Cog):
                 color=disnake.Color.dark_red()
                 )
             await ctx.send(embed=embed)
-
-    @commands.command(name="toggle", pass_context=True)
-    @commands.is_owner()
-    async def toggle(self, ctx, *, command):
-        command = self.client.get_command(command)
-
-        if command is None:
-            embed = disnake.Embed(title="ERROR", description="I can't find a command with that name!", color=0xff0000)
-            await ctx.send(embed=embed)
-
-        elif ctx.command == command:
-            embed = disnake.Embed(title="ERROR", description="You cannot disable this command.", color=0xff0000)
-            await ctx.send(embed=embed)
-
-        else:
-            command.enabled = not command.enabled
-            ternary = "enabled" if command.enabled else "disabled"
-            embed = disnake.Embed(title="Toggle", description=f"I have {ternary} {command.qualified_name} for you!", color=0xff00c8)
-            await ctx.send(embed=embed)
-
-    @commands.command(pass_context=True)
-    @commands.is_owner()
-    async def leave(self, ctx, *, guildinput):
-        try:
-            guildid = int(guildinput)
-        except:
-            await ctx.send("Invalid guild: failed to convert to int")
-
-        try:
-            guild = self.client.get_guild(guildid)
-        except:
-            await ctx.send("Invalid guild")
-
-        try:
-            await guild.leave()
-            await ctx.send(f"left {guild.name}")
-        except:
-            await ctx.send("Error leaving")
-
-    @commands.command(pass_context=True)
-    @commands.is_owner()
-    async def servers(self, ctx):
-        await ctx.send(f"{len(self.client.guilds)}")
-        listofids = []
-        for guild in self.client.guilds:
-            listofids.append(guild.id)
-        await ctx.send(listofids)
 
 def setup(client):
     client.add_cog(Utility(client))
