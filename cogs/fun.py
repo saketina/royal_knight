ce0bc235f725f2a0b41b5ad3f9c6e23a73e027a5
Saketina <deni.dobrijevic@gmail.com>
2023-07-05 16:38:49 +0200

    changed FIXME to BUG

--- cogs/fun.py
+++ cogs/fun.py
@@ -8,7 +8,7 @@ from disnake.ext import commands
 
 # //TODO remove unneeded imports and lines of code
 # //TODO check for help message is variables are blank
-# //FIXME rps is fucked idk where or when
+# //BUG rps is fucked idk where or when
 
 class Fun(commands.Cog):
 
