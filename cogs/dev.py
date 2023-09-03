6db5c6c88503519b281cfd772f4dd957daced916
Saketina <deni.dobrijevic@gmail.com>
2023-09-04 00:51:51 +0200

    fixing dev cmds

--- cogs/dev.py
+++ cogs/dev.py
@@ -20,115 +20,78 @@ class Dev(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def cls(self, ctx):
-        os.system("cls")
-        await ctx.send("Cleared the terminal, Daddy.")
+        try:
+            os.system("cls")
+            await ctx.send("Cleared the terminal, Daddy.")
+        except commands.NotOwner:
+            return
+        except Exception as e:
+            print(f"Error: \nType: {type(e).__name__} \nInfo - {e}")
 
     
     @commands.command()
     @commands.is_owner()
-    async def delwarns(self, ctx, member:disnake.Member=None):
-        if member != None:
-            try:
-                db_warns = db.child("MODERATIONS").child("WARNS").child(ctx.guild.id).child(member.id).get().val()
-                if db_warns != None:
-                    button_yes = Button(label="Yes", style=disnake.ButtonStyle.green)
-                    button_no = Button(label="No", style=disnake.ButtonStyle.red)
+    async def delmods(self, ctx, member:disnake.Member=None):
+        try:
+            if member != None:
+                try:
+                    db_warns = db.child("MODERATIONS").child("WARNS").child(ctx.guild.id).child(member.id).get().val()
+                    if db_warns != None:
+                        button_yes = Button(label="Yes", style=disnake.ButtonStyle.green)
+                        button_no = Button(label="No", style=disnake.ButtonStyle.red)
 
-                    async def button_yes_callback(interaction):
-                        if interaction.author.id == ctx.author.id:
-                            db.child("MODERATIONS").child("WARNS").child(ctx.guild.id).child(member.id).remove()
-                            await interaction.response.edit_message(content="All warns have been deleted", view=None)
-                        else:
-                            return
+                        async def button_yes_callback(interaction):
+                            if interaction.author.id == ctx.author.id:
+                                db.child("MODERATIONS").child("WARNS").child(ctx.guild.id).child(member.id).remove()
+                                await interaction.response.edit_message(content="All warns have been deleted", view=None)
+                            else:
+                                return
 
-                    async def button_no_callback(interaction):
-                        if interaction.author.id == ctx.author.id:
-                            await interaction.response.edit_message(content="I didn\'t delete any warn", view=None)
-                        else:
-                            return
+                        async def button_no_callback(interaction):
+                            if interaction.author.id == ctx.author.id:
+                                await interaction.response.edit_message(content="I didn\'t delete any warn", view=None)
+                            else:
+                                return
 
-                    button_yes.callback = button_yes_callback
-                    button_no.callback = button_no_callback
+                        button_yes.callback = button_yes_callback
+                        button_no.callback = button_no_callback
 
-                    view = View(timeout=30)
-                    view.add_item(button_yes)
-                    view.add_item(button_no)
-                    msg = await ctx.send("Are you sure?", view=view)
+                        view = View(timeout=30)
+                        view.add_item(button_yes)
+                        view.add_item(button_no)
+                        msg = await ctx.send("Are you sure?", view=view)
 
 
-                else:
-                    await ctx.send("There aren\'t any moderations to delete")
-            except:
-                await ctx.send("There was an error. Please contact the dev.")
-        else:
-            await ctx.send("Please input a member.")
-    """
-    @commands.command(pass_context=True)
-    #@has_permissions(administrator=True)
-    async def status(self, ctx, activity=None, *, text=None):
-        if activity == "playing":
-            act = disnake.Activity(type=disnake.ActivityType.playing, name=text)
-            response = "Playing"
-            await self.client.change_presence(activity=act, status=disnake.Status.dnd)
-            await ctx.send(f"Status changed to `{response} {text}`")
-
-        elif activity == "streaming":
-            act = disnake.Activity(type=disnake.ActivityType.streaming, name=text)
-            response = "Streaming"
-            await self.client.change_presence(activity=act, status=disnake.Status.dnd)
-            await ctx.send(f"Status changed to `{response} {text}`")
-
-        elif activity == "watching":
-            act = disnake.Activity(type=disnake.ActivityType.watching, name=text)
-            response = "Watching"
-            await self.client.change_presence(activity=act, status=disnake.Status.dnd)
-            await ctx.send(f"Status changed to `{response} {text}`")
-
-        elif activity == "competing":
-            act = disnake.Activity(type=disnake.ActivityType.competing, name=text)
-            response = "Competing in"
-            await self.client.change_presence(activity=act, status=disnake.Status.dnd)
-            await ctx.send(f"Status changed to `{response} {text}`")
-
-        elif activity == "remove":
-            act = disnake.Activity(type=disnake.ActivityType.custom, name=None)
-            await self.client.change_presence(activity=act, status=disnake.Status.dnd)
-            await ctx.send(f"Status changed to `{text}`")
-
-        elif activity==None or text==None:
-            embed = disnake.Embed(
-            title="STATUS HELP",
-            description="``k.status [activity] [text]``",
-            color = disnake.Color.dark_red()
-            )
-            embed.add_field(
-            name="Activity",
-            value="playing" + "\n" +
-                  "streaming" + "\n" +
-                  "watching" + "\n" +
-                  "competing" + "\n" +
-                  "remove"
-            )
-
-            await ctx.send(embed=embed)
-        else:
-            await ctx.send("I didn\'t quite catch that.")
-    """
+                    else:
+                        await ctx.send("There aren\'t any moderations to delete")
+                except:
+                    await ctx.send("There was an error. Please contact the dev.")
+            else:
+                await ctx.send("Please input a member.")
+        except commands.NotOwner:
+            return
+        except Exception as e:
+            print(f"Error: \nType: {type(e).__name__} \nInfo - {e}")
 
     @commands.command(pass_context=True)
     @commands.is_owner()
     async def say(self, ctx, *, message):
-        if message != None:
-             
-            if str(ctx.guild.default_role) not in message and "@here" not in message:
-                msg = message
-            else:
-                msg = "You can\'t make me say that"
-            
-            await ctx.message.delete()
-            await ctx.send(msg)
-        else: 
-            await ctx.send("Please tell me what to say")
+        try:
+            if message != None:
+                
+                if str(ctx.guild.default_role) not in message and "@here" not in message:
+                    msg = message
+                else:
+                    msg = "You can\'t make me say that"
+                
+                await ctx.message.delete()
+                await ctx.send(msg)
+            else: 
+                await ctx.send("Please tell me what to say")
+        except commands.NotOwner:
+            return
+        except Exception as e:
+            print(f"Error: \nType: {type(e).__name__} \nInfo - {e}")
 
         
     @commands.command(name='eval', pass_context=True)
@@ -141,6 +104,8 @@ class Dev(commands.Cog):
             else:
                 ans = eval(expr)
             await ctx.send(f"Answer: {ans}")
+        except commands.NotOwner:
+            return
         except Exception as e:
             print(e.__traceback__)
             await ctx.send("Didn't work.")
@@ -148,49 +113,64 @@ class Dev(commands.Cog):
     @commands.command(name="toggle", pass_context=True)
     @commands.is_owner()
     async def toggle(self, ctx, *, command):
-        command = self.client.get_command(command)
+        try:
+            command = self.client.get_command(command)
 
-        if command is None:
-            embed = disnake.Embed(title="ERROR", description="I can't find a command with that name!", color=0xff0000)
-            await ctx.send(embed=embed)
+            if command is None:
+                embed = disnake.Embed(title="ERROR", description="I can't find a command with that name!", color=0xff0000)
+                await ctx.send(embed=embed)
 
-        elif ctx.command == command:
-            embed = disnake.Embed(title="ERROR", description="You cannot disable this command.", color=0xff0000)
-            await ctx.send(embed=embed)
+            elif ctx.command == command:
+                embed = disnake.Embed(title="ERROR", description="You cannot disable this command.", color=0xff0000)
+                await ctx.send(embed=embed)
 
-        else:
-            command.enabled = not command.enabled
-            ternary = "enabled" if command.enabled else "disabled"
-            embed = disnake.Embed(title="Toggle", description=f"I have {ternary} {command.qualified_name} for you!", color=0xff00c8)
-            await ctx.send(embed=embed)
+            else:
+                command.enabled = not command.enabled
+                ternary = "enabled" if command.enabled else "disabled"
+                embed = disnake.Embed(title="Toggle", description=f"I have {ternary} {command.qualified_name} for you!", color=0xff00c8)
+                await ctx.send(embed=embed)
+        except commands.NotOwner:
+            return
+        except Exception as e:
+            print(f"Error: \nType: {type(e).__name__} \nInfo - {e}")
 
     @commands.command(pass_context=True)
     @commands.is_owner()
     async def leave(self, ctx, *, guildinput):
         try:
-            guildid = int(guildinput)
-        except:
-            await ctx.send("Invalid guild: failed to convert to int")
+            try:
+                guildid = int(guildinput)
+            except:
+                await ctx.send("Invalid guild: failed to convert to int")
 
-        try:
-            guild = self.client.get_guild(guildid)
-        except:
-            await ctx.send("Invalid guild")
+            try:
+                guild = self.client.get_guild(guildid)
+            except:
+                await ctx.send("Invalid guild")
 
-        try:
-            await guild.leave()
-            await ctx.send(f"left {guild.name}")
-        except:
-            await ctx.send("Error leaving")
+            try:
+                await guild.leave()
+                await ctx.send(f"left {guild.name}")
+            except:
+                await ctx.send("Error leaving")
+        except commands.NotOwner:
+            return
+        except Exception as e:
+            print(f"Error: \nType: {type(e).__name__} \nInfo - {e}")
 
     @commands.command(pass_context=True)
     @commands.is_owner()
     async def servers(self, ctx):
-        await ctx.send(f"{len(self.client.guilds)}")
-        listofids = []
-        for guild in self.client.guilds:
-            listofids.append(guild.id)
-        await ctx.send(listofids)
+        try:
+            await ctx.send(f"{len(self.client.guilds)}")
+            listofids = []
+            for guild in self.client.guilds:
+                listofids.append(guild.id)
+            await ctx.send(listofids)
+        except commands.NotOwner:
+            return
+        except Exception as e:
+            print(f"Error: \nType: {type(e).__name__} \nInfo - {e}")
 
 def setup(client):
     client.add_cog(Dev(client))
