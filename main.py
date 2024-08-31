609eea03b18665d5cf33b505505c99d6435a81ab
Saketina <deni.dobrijevic@gmail.com>
2024-08-31 21:12:09 +0200

    TEST: new way of logging stuff

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import datetime
 import json
 import os
 import sys
 import time
 import traceback
-from datetime import datetime
+import datetime
+from datetime import datetime as dt
 
 import subprocess
 
@@ -18,6 +18,7 @@ import logging
 
 #?# TODO optimize code so less data is stored in memory and more data is stored locally, would improve speed and efficiency
 
+start_time = time.time()
 
 if os.name == "nt":
     log_file_path = 'logs/client.log'
@@ -25,21 +26,25 @@ elif os.name == "posix" :
     log_file_path = '~/logs/client.log'
 else:
     log_file_path = 'logs/client.log'
-logging.basicConfig(level=logging.INFO,
+"""logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s: %(name)s: %(levelname)s: %(message)s',
                     datefmt='%m-%d %H:%M',
                     filename=log_file_path,
                     filemode='w')
-
-logger = logging.StreamHandler()
+"""
+logger = logging.getLogger('disnake')
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(name)s: %(levelname)s: %(message)s')
-logger.setFormatter(formatter)
-logging.getLogger('').addHandler(logger)
+file_handler = logging.FileHandler(filename=log_file_path, encoding='utf-8', mode='w')
+file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+terminal_handler = logging.StreamHandler()
+file_handler.setFormatter(logging.Formatter('%(levelname)s:%(name)-15s: %(message)s'))
+logger.addHandler(file_handler)
+logger.addHandler(terminal_handler)
 
 cog_counter = 0
 
 intents = disnake.Intents.all()
+intents.presences=False
 
 def get_prefix(client, message):
     prefixes = ["k.", "K.", "<@850019720648589352>"]
@@ -62,8 +67,9 @@ client.remove_command("help")
 @client.event
 async def on_ready():
     logging.info(
-        f"\nLogged in as: {client.user.name} - {client.user.id}\nWrapper Version: {disnake.__version__}\nAt: {datetime.now()}\n"
+        f"\nLogged in as: {client.user.name} - {client.user.id}\nWrapper Version: {disnake.__version__}\nAt: {dt.now()}\n"
     )
+    pass
 
 @client.command()
 @is_owner()
@@ -82,10 +88,33 @@ async def restart(ctx):
 @is_owner()
 async def update(ctx):
     embed = disnake.Embed(
-        title="Updated the current host server",
+        description="Updated the current host server",
         color=disnake.Color.dark_red()
     )
     os.system("git pull")
+    await ctx.send(embed=embed)
+    
+@client.command()
+async def info(ctx):
+    embed = disnake.Embed(
+        title="General Information",
+        color=disnake.Color.dark_red(),
+        timestamp=dt.now()
+    )
+    current_time = time.time()
+    difference = int(round(current_time - start_time))
+    text = str(datetime.timedelta(seconds=difference))
+    embed.add_field(
+        name="Bot info",
+        value=f"Uptime: {text}\n"
+                f"Shard ID: {ctx.guild.shard_id}\n"
+                #f"Currently serving {len(self.client.guilds)} servers"
+    )
+    embed.add_field(
+        name="Developer",
+        value=f"Discord: thecrazydragon({client.owner.mention})"
+    )
+    #embed.set_thumbnail(client.owner.avatar)
     await ctx.send(embed=embed)
 
 initial_cogs = [
--- settings.py
+++ settings.py
@@ -2,38 +2,66 @@ import os
 import logging
 from logging.config import dictConfig
 
+if os.name == "nt":
+    client_log_file_path = 'logs/client.log'
+    debug_log_file_fath = 'logs/debug.log'
+elif os.name == "posix" :
+    log_file_path = '/home/admin/logs/client.log'
+    debug_log_file_fath = '/home/admin/logs/debug.log'
+else:
+    log_file_path = 'logs/client.log'
+    debug_log_file_fath = 'logs/debug.log'
+
 LOGGING_CONFIG = {
     "version": 1,
     "disabled_existing_loggers": False,
     "formatters": {
         "verbose": {
-            "format": "%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s"
+            "format": " %(asctime)-1s %(levelname)s - %(module)-1s : %(message)s"
         },
-        "standard": {"format": "%(levelname)-10s - %(name)-15s : %(message)s"},
+        "standard": {
+            "format": "%(levelname)-1s - %(name)-1s : %(message)s"
+            },
     },
     "handlers": {
         "console": {
-            "level": "DEBUG",
+            "level": "INFO",
             "class": "logging.StreamHandler",
             "formatter": "standard",
         },
         "console2": {
-            "level": "WARNING",
+            "level": "DEBUG",
             "class": "logging.StreamHandler",
-            "formatter": "standard",
+            "formatter": "verbose",
         },
         "file": {
             "level": "INFO",
             "class": "logging.FileHandler",
-            "filename": "~/logs/infos.log",
+            "filename": client_log_file_path,
+            "mode": "w",
+            "formatter": "standard",
+        },
+        "file2": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": debug_log_file_fath,
             "mode": "w",
             "formatter": "verbose",
         },
     },
     "loggers": {
-        "client": {"handlers": ["console"], "level": "INFO", "propagate": False},
+        "client": {
+            "handlers": ["console", "file"],
+            "level": "INFO",
+            "propagate": False
+            },
+        "debug": {
+            "handlers": ["console2", "file2"],
+            "level": "DEBUG",
+            "propagate": False
+            },
         "discord": {
-            "handlers": ["console2", "file"],
+            "handlers": ["console2", "file2"],
             "level": "INFO",
             "propagate": False,
         },
