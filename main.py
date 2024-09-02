bd05237a1c1a4cd769b0e78266b2a1177990f0b4
Saketina <deni.dobrijevic@gmail.com>
2024-09-02 23:08:58 +0200

    :)

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ elif os.name == "posix" :
     os.mkdir(f"/home/admin/logs/{dt.date()}-rk.log")
     log_file_path = f"/home/admin/logs/{dt.date()}-rk.log"
 else:
-    log_file_path = 'logs/client.log'
+    log_file_path = 'logs/client-else.log'
 """logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s: %(name)s: %(levelname)s: %(message)s',
                     datefmt='%m-%d %H:%M',
